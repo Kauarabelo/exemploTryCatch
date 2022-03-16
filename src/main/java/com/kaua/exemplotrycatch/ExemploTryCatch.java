1e5c3fd96c134f31119a664f800bb6e9611eb422
Kauarabelo <kauarabelo1@gmail.com>
2022-03-16 11:25:25 -0300

    Adicionado GitHelloApp com a main() - v1.0

--- src/main/java/com/kaua/exemplotrycatch/ExemploTryCatch.java
+++ src/main/java/com/kaua/exemplotrycatch/ExemploTryCatch.java
@@ -4,11 +4,37 @@
  * and open the template in the editor.
  */
 package com.kaua.exemplotrycatch;
+import java.util.Scanner;
 
 /**
  *
  * @author devsys-b
  */
 public class ExemploTryCatch {
+    public static void main(String[] args) {
+        
+        int [] valor = {10,20,30,40};
+        boolean saida = true;
+        Scanner teclado = new Scanner(System.in);
+        
+        do{
+            try{
+                System.out.println("\nDigite um valor: ");
+                int a = teclado.nextInt();
+                
+                System.out.print(valor[a]);
+                
+                saida = false;
+                
+            }
+            catch (ArrayIndexOutOfBoundsException arrayIndexOutOfBoundsException){
+                System.err.printf("%nErro: %s%n",arrayIndexOutOfBoundsException);
+                System.out.printf("%n Valor de busca do array maior que o alocado");
+                saida = false;
+
+            }
+        }while(saida);    
+       
+    }
     
 }
